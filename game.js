a77f586a57eeade7dd44697314d740eda408e235
emachaffie <emachaffie@gmail.com>
2018-05-10 16:54:04 -0400

    Numbers one and two working, for now

--- game.js
+++ game.js
@@ -1,25 +1,37 @@
-
-
-// ### Step 1
-
-//  Create a 500x500 `canvas` tag in your HTML for the game. Create a `Game` class that takes a canvas node and saves it. This class should have a method `draw` that draws a "hollow" (10px white line, no fill) 200x200 square in the middle of the canvas.
-
-// This class should also have a method `tick` that calls the `draw` method and uses `requestAnimationFrame` to call `tick` again.
-
 class Game {
-    constructor (canvas) {
-        
-        function draw(screen, ?) {
-            var canvas = document.getElementById("game-canvas");
-            var screen = canvas.getContext('2d');
-
-
-        }
-
-        tick (draw) {
-        requestAnimationFrame(this)
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.screen = canvas.getContext('2d');
+        widgets = [];
+        this.player = new Player(this);
     }
+
+    draw() {
+        this.screen.strokeStyle = "#FFF";
+        this.screen.lineWidth = 10;
+        this.screen.strokeRect(150, 150, 200, 200);
+
+        this.screen.font = "40px Arial";
+        this.screen.fillStyle = "#F1B72A";
+        this.screen.textAlign = "center";
+        this.screen.fillText("Snitch Snatch", 250, 50);
+
+        this.player.draw();
     }
+
+    tick() {
+        this.draw()
+        // console.log("ticky")
+        requestAnimationFrame(this.tick.bind(this))
+    }
+
+    update() {
+
+    }
+
+    //make empty array for objects? in constructor?
+    //create a PLayer
+    //draw player
 }
 
 // ### Step 2
@@ -27,12 +39,69 @@ class Game {
 // Create a `Player` class. This class should have a method `draw` that draws 
 // a 40x40 square in the middle of the canvas. The `Game` class's `draw` method should call the `Player` class's `draw` method.
 
+class Player {
+    constructor(game) {
+        this.game = game
+    }
+    draw() {
+        this.game.screen.fillStyle = "#F1B72A";
+        this.game.screen.fillRect(230, 230, 40, 40);
+    }
+    update() {
+        
+    }
+
+}
+
+
+
 // ### Step 3
 
 // Add movement to the player's rectangle. The rectangle should not be allowed to move outside the containing hollow rectangle.
 
 // Create a `Keyboarder` class. You can use the one from [Cook's annotated Space Invaders](http://annotated-code.maryrosecook.com/space-invaders/docs/space-invaders.html). Use your keyboarder to make the player's rectangle move. To do this, you will need to add an `update` method to the `Game` class, and an `update` method to the `Player` class.
 
+class Keyboarder {
+    constructor() {
+        this.keyState = {}
+
+        window.addEventListener('keydown', function (e) {
+            this.keyState[e.keyCode] = true
+        }.bind(this))
+
+        window.addEventListener('keyup', function (e) {
+            this.keyState[e.keyCode] = false
+        }.bind(this))
+    }
+
+    isDown(keyCode) {
+        return this.keyState[keyCode] === true
+    }
+
+    on(keyCode, callback) {
+        window.addEventListener('keydown', function (e) {
+            if (e.keyCode === keyCode) {
+                callback()
+            }
+        })
+    }
+}
+
+Keyboarder.KEYS = {
+    LEFT: 37,
+    RIGHT: 39,
+    UP: 38,
+    DOWN: 40,
+    S: 83
+};
+
+
+
+var canvas = document.getElementById("game-canvas");
+// window.addEventListener('load', function() {
+var game = new Game(canvas);
+game.tick()
+//   });
 // ### Step 4
 
 // Add a coin for the player to gather to the game. This coin, like in the example game, should appear in a random position that is not the player's current position. When the player collects it, it should disappear and reappear elsewhere.
