8391cca23904617737e5bcf4686e7baa2875447c
emachaffie <emachaffie@gmail.com>
2018-05-11 15:23:50 -0400

    Snitch appearing randomly but sometimes on player, collision inaccurate but functional

--- game.js
+++ game.js
@@ -3,16 +3,23 @@ class Game {
         this.canvas = canvas;
         this.screen = canvas.getContext('2d');
         widgets = [];
-        this.gameSize = {x:canvas.width, y: canvas.height}
+        this.gameSize = {
+            x: canvas.width,
+            y: canvas.height
+        }
         this.player = new Player(this);
         this.keyboarder = new Keyboarder();
-        this.snitch = new Snitch();
-        this.beater = new Beater;
+        this.snitch = new Snitch(this);
+        // this.beater = new Beater();
     }
 
     update() {
+
+        if (colliding (this.player, this.snitch)) {
+            this.snitch.update();
+            }//detect collisions from ?
         this.player.update();
-        // this.snitch.update();
+        
         // this.beater.update();
     }
 
@@ -20,7 +27,7 @@ class Game {
         //can clear by covering previous with background color,OR using clearRect
         // screen.clearRect(0, 0, gameSize.x, gameSize.y);
         this.screen.clearRect(0, 0, this.gameSize.x, this.gameSize.y)
-        
+
 
         this.screen.strokeStyle = "#FFF";
         this.screen.lineWidth = 10;
@@ -32,7 +39,7 @@ class Game {
         this.screen.fillText("Snitch Snatch", 250, 50);
 
         this.player.draw();
-        // this.snitch.draw();
+        this.snitch.draw();
         // this.beater.draw();
     }
 
@@ -44,49 +51,35 @@ class Game {
     }
 }
 
-// ### Step 2
-
-// Create a `Player` class. This class should have a method `draw` that draws 
-// a 40x40 square in the middle of the canvas. The `Game` class's `draw` method should call the `Player` class's `draw` method.
-
 class Player {
     constructor(game) {
         this.game = game;
         this.Keyboarder = new Keyboarder();
-        this.center = {x:250, y:250};
-        this.size = {x:40, y:40};
+        this.center = {
+            x: 250,
+            y: 250
+        };
+        this.size = {
+            x: 40,
+            y: 40
+        };
     }
     update() {
-        if (this.Keyboarder.isDown(Keyboarder.KEYS.LEFT)) {
-            this.center.x -= 10;
-        } else if (this.Keyboarder.isDown(Keyboarder.KEYS.RIGHT)) {
-            this.center.x += 10;
-        } else if (this.Keyboarder.isDown(Keyboarder.KEYS.UP)) {
-            this.center.y -= 10;
-        } else if (this.Keyboarder.isDown(Keyboarder.KEYS.DOWN)) {
-            this.center.y += 10;
+        if (this.Keyboarder.isDown(Keyboarder.KEYS.LEFT) && this.center.x > 181) {
+            this.center.x -= 5;
+        } else if (this.Keyboarder.isDown(Keyboarder.KEYS.RIGHT) && this.center.x < 320) {
+            this.center.x += 5;
+        } else if (this.Keyboarder.isDown(Keyboarder.KEYS.UP) && this.center.y > 181) {
+            this.center.y -= 5;
+        } else if (this.Keyboarder.isDown(Keyboarder.KEYS.DOWN) && this.center.y < 320) {
+            this.center.y += 5;
         }
     }
     draw() {
         this.game.screen.fillStyle = "#F1B72A";
-        this.game.screen.fillRect(this.center.x-this.size.x/2, this.center.y-this.size.y/2, this.size.x, this.size.y);
+        this.game.screen.fillRect(this.center.x - this.size.x / 2, this.center.y - this.size.y / 2, this.size.x, this.size.y);
     }
-
 }
-
-// NOTES FROM WHAT IM DOING THURS NIGHT: 
-// Added new Keyboarder to Player constructor and Game constructor
-// Added update methods ABOVE draw fields for both Player and Game classes
-// Added update methods to Player, including addition of UP and DOWN
-// Added classes Snitch (Coin) and Beater (bullet?)
-
-
-
-// ### Step 3
-
-// Add movement to the player's rectangle. The rectangle should not be allowed to move outside the containing hollow rectangle.
-
-// Create a `Keyboarder` class. You can use the one from [Cook's annotated Space Invaders](http://annotated-code.maryrosecook.com/space-invaders/docs/space-invaders.html). Use your keyboarder to make the player's rectangle move. To do this, you will need to add an `update` method to the `Game` class, and an `update` method to the `Player` class.
 
 class Keyboarder {
     constructor() {
@@ -132,24 +125,35 @@ Keyboarder.KEYS = {
 
 // To do this, you will likely want to create a `Coin` class.
 
+
+//"Coin"
 class Snitch {
     constructor(game) {
         this.game = game;
-        // this.size = 
-        // this.center = 
+        this.center = {x:155, y:155};
+        this.size = {
+            x: 20,
+            y: 20
+        };
     }
 
-    // update() {
-
-    // }
+    update() {
+        //if no collision, {
+        //     don't change position
+        // } else {}
+        this.center.x = Math.floor(Math.random() * (310 - 191 + 1)) + 191;
+        this.center.y = Math.floor(Math.random() * (310 - 191 + 1)) + 191;
+        // console.log("snitch")
+    }
 
     draw() {
         this.game.screen.fillStyle = "#F1B72A";
-        // this.game.screen.fillRect(0, 0, 20, 20);
+        this.game.screen.fillRect(this.center.x, this.center.y, this.size.x, this.size.y);
     }
 
     // score() {
-    
+    // when collision with Player, 
+    //  score += score
     // }
 }
 
@@ -157,17 +161,27 @@ class Snitch {
 
 // Add hazards to the game. These hazards work like in the example game: flying squares that come from a random edge of the canvas and travel to the opposite edge. They should cross through the outer rectangle that encloses the player. If the player and a hazard collide, reset the score.
 
-class Beater {
-    constructor(game) {
-        this.game = game;
-        // this.size = 
-        // this.center = 
-    }
-    draw() {
-        this.game.screen.fillStyle = "#2a623d";
-        // this.game.screen.fillRect(20, 20, 0, 250);
-    }
-}
+// class Beater {
+//     constructor(game) {
+//         this.game = game;
+//         // this.size = 
+//         // this.center = 
+//     }
+//     draw() {
+//         this.game.screen.fillStyle = "#2a623d";
+//         // this.game.screen.fillRect(20, 20, 0, 250);
+//     }
+// }
+
+var colliding = function (b1, b2) {
+    return !(
+        b1 === b2 ||
+        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+        b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+    );
+};
 
 var canvas = document.getElementById("game-canvas");
 window.addEventListener('load', function () {
